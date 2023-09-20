6ea4763bc931e2f448380930971973d711073c1e
Nazerke <ybraeva.n@gmail.com>
2023-09-20 15:14:16 +0600

    Array Library

---
+++ example008_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 19, 52, 73, 44, 50, 65, 77, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
